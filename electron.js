fc56cdf40333d8e4931cb9c7f1e2d0e8ca1a5ff7
Yoshiki Aoki <ringohub@gmail.com>
2018-09-10 01:16:37 +0900

    Update electron.js

--- electron.js
+++ electron.js
@@ -27,7 +27,7 @@ function createWindow() {
 
   // and load the index.html of the app.
   const startUrl = process.env.ELECTRON_START_URL || url.format({
-    pathname: path.join(__dirname, '/../build/index.html'),
+    pathname: path.join(__dirname, './build/index.html'),
     protocol: 'file:',
     slashes: true
   });
